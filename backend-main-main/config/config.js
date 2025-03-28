14a3c8b7d974be77f8cc171fb0205844f49b298a
Maryam Abdelraheeem <140979365+MaryamAbdelraheem@users.noreply.github.com>
2025-03-28 13:14:32 +0200

    Add files via upload

---
+++ backend-main-main/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    development: {
+        username: 'root',
+        password: '123456789',
+        database: 'obstructivesleepapneadb',
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        port: 3306
+    },
+    test: {
+        // إعدادات البيئة الاختبارية
+    },
+    production: {
+        // إعدادات البيئة الإنتاجية
+    }
+};
