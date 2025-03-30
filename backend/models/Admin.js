7fd240b6a6c1078d5bf8aec0c1970f655602196e
Maryam Abdelraheeem <140979365+MaryamAbdelraheem@users.noreply.github.com>
2025-03-30 04:58:17 +0200

    Delete backend directory

--- backend/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const dbPromise = require("./database");
-
-async function getAdminByEmail(email) {
-  const db = await dbPromise;
-  return db.get(`SELECT * FROM admin WHERE email = ?`, [email]);
-}
-
-module.exports = { getAdminByEmail };
