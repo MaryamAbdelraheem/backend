ac328867b35e05007b8a5397d8fc01bd156b3c75
Maryam Abdelraheeem <140979365+MaryamAbdelraheem@users.noreply.github.com>
2025-04-16 18:07:54 +0200

    Delete backend-main-main directory

--- backend-main-main/util/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize'); 
-
-const sequelize = new Sequelize('obstructivesleepapneadb', 'root', '123456789', {
-    dialect: 'mysql',
-    host: 'localhost',
-    // dialectModule: require('mysql2'), // تحديد استخدام mysql2
-});
-
-module.exports = sequelize;
